94375bd00971344d3db1a15055ef670a40c3774d
Antonio <idiezag@gmail.com>
2022-04-11 19:26:40 -0400

    Creating functions called Debits and Credits

--- src/App.js
+++ src/App.js
@@ -2,13 +2,31 @@ import logo from './logo.svg';
 import './App.css';
 import Clock from './components/Clock';
 import Greeting  from './components/Greeting';
+import Credits from './components/Credits';
+import Debits from './components/Debits';
+import React,{useState} from 'react'
 
 function App() {
   return (
-    <div>
-      <Clock/>
-      <Greeting/>
-    </div>
+    
+    <body>
+      <div>
+       <Clock/>
+      </div>
+      
+      <div>
+        <Greeting/>
+      </div>
+      
+      <div>
+        <Credits/>
+      </div>
+      
+      <div>
+        <Debits/>
+      </div>
+      
+    </body>
   );
 }
 
